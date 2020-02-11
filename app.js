906debef99c5006d185a84ff3e23427fee672d66
dolev <dolevizacson@gmail.com>
2020-02-11 20:41:04 +0200

    add mailer agent initialization and contact endpoint

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ modules.DOTENV.config();
 const mongoInit = require(files.MONGO);
 const errorHandlers = require(files.ERROR_HANDLERS);
 const middleware = require(files.MIDDLEWARE);
+const mailerInit = require(files.MAILER_AGENT);
 
 // errors
 const BadEndpointError = require(files.BAD_ENDPOINT_ERROR);
@@ -16,12 +17,16 @@ const BadEndpointError = require(files.BAD_ENDPOINT_ERROR);
 // db connections
 mongoInit();
 
+// mailer init
+mailerInit();
+
 // controllers
 const auth = require(files.AUTH);
 const skills = require(files.SKILLS);
 const projects = require(files.PROJECTS);
 const blog = require(files.BLOG);
 const taskList = require(files.TASK_LIST);
+const mailer = require(files.MAILER);
 
 process.on('uncaughtException', function(err) {
   //mailer logic to report on problems with the app
@@ -37,6 +42,7 @@ app.use(routes.SKILLS, skills);
 app.use(routes.PROJECTS, projects);
 app.use(routes.BLOG, blog);
 app.use(routes.TASK_LIST, taskList);
+app.use(routes.CONTACT, mailer);
 
 // default route handler
 app.get('*', (req, res, next) => {
